080c1236dc770ce80ae57e7ff649352c58fb5bc5
Edward Casbon <edward@edwardcasbon.co.uk>
2015-04-02 15:33:05 +0100

    Continued building out the attachments controller.

--- app/modules/default/controllers/AttachmentsController.php
+++ app/modules/default/controllers/AttachmentsController.php
@@ -1,11 +1,34 @@
 <?php
 class AttachmentsController extends Bucketlists_Controller {
 	
+	public function __construct ($route) {
+		
+		// Call the Bucketlists constructor.
+		parent::__construct($route);
+		
+		// Create an instance of the model.
+		$this->attachmentsModel = new Attachments($this->db);		
+	}
+	
 	public function indexAction () {
 
 		// Validate the HTTP method.
 		$this->validateHttpMethod("GET");
+		
+		// Expected / Allowed params.
+		$expectedParams = array(
+			"id",
+			"itemId"
+		);
+		
+		// Get the params	
+		$params = $this->getExpectedParams($expectedParams);
+				
+		// Get the attachments.
+		$attachments = $this->attachmentsModel->get($this->format($params));
 			
+		// Output the attachments.
+		$this->view->json = $attachments;
 	}
 	
 	public function createAction () {
@@ -13,6 +36,14 @@ class AttachmentsController extends Bucketlists_Controller {
 		// Validate the HTTP method.
 		$this->validateHttpMethod("POST");
 		
+		// Format the params.
+		$params = $this->format($this->route->params);
+				
+		// Create the attachment.
+		$attachment = $this->attachmentsModel->add($params);
+								
+		// Output the attachment.
+		$this->view->json = $attachment;		
 	}
 	
 	public function updateAction () {
