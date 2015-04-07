f09b54e6d4150ca44222b503a163e9168b6992c6
Edward Casbon <edward@edwardcasbon.co.uk>
2015-04-07 12:00:01 +0100

    Items func.

--- app/modules/default/controllers/ItemsController.php
+++ app/modules/default/controllers/ItemsController.php
@@ -1,35 +1,99 @@
 <?php
 class ItemsController extends Bucketlists_Controller {
-	
+
+	public function __construct ($route) {
+
+		// Call the Bucketlists constructor.
+		parent::__construct($route);
+
+		// Create an instance of the model.
+		$this->itemsModel = new Items($this->db);
+	}
+
 	public function indexAction () {
 
 		// Validate the HTTP method.
 		$this->validateHttpMethod("GET");
-			
+
+		// Expected / Allowed params.
+		$expectedParams = array(
+			"id",
+			"bucketId",
+			"tagId"
+		);
+
+		// Get the params
+		$params = $this->getExpectedParams($expectedParams);
+
+		// Get the items.
+		$items = $this->itemsModel->get($this->format($params));
+
+		// Output the items.
+		$this->view->json = $items;
 	}
-	
+
 	public function createAction () {
-		
+
 		// Validate the HTTP method.
 		$this->validateHttpMethod("POST");
-		
+
+		// Format the params.
+		$params = $this->format($this->route->params);
+
+		// Create the item.
+		$item = $this->itemsModel->add($params);
+
+		// Output the item.
+		$this->view->json = $item;
 	}
-	
+
 	public function updateAction () {
-		
+
 		// Validate the HTTP method.
 		$this->validateHttpMethod("PUT");
-		
+
+		// Expected / Allowed params.
+		$expectedParams = array(
+			"id"
+		);
+
+		// Get the expected params.
+		$this->getExpectedParams($expectedParams);
+
+		// Get the params.
+		$params = $this->format($this->route->params);
+
+		// Get the criteria (ID).
+		$criteria = array("id" => $params["id"]);
+
+		// Get the data, by removing the ID.
+		unset($params["id"]);
+
+		// Update the item.
+		$item = $this->itemsModel->update($params, $criteria);
+
+		// Output the updated item.
+		$this->view->json = $item;
 	}
-	
+
 	public function deleteAction () {
-		
+
 		// Validate the HTTP method.
 		$this->validateHttpMethod("DELETE");
-		
-		// Remember to update the status code of the record, rather than delete 
-		// the record entirely!
-		
+
+		// Expected / Allowed params.
+		$expectedParams = array(
+			"id"
+		);
+
+		// Get the expected params.
+		$params = $this->getExpectedParams($expectedParams);
+
+		// Update the items status code, rather than delete it completely.
+		$item = $this->itemsModel->update(array("status" => 0), array("id" => $params["id"]));
+
+		// Output the updated item.
+		$this->view->json = $item;
 	}
-	
+
 }
--- app/modules/default/models/Items.php
+++ app/modules/default/models/Items.php
@@ -1,6 +1,41 @@
 <?php
 class Items extends Bucketlists_Model {
-	
+
 	protected $tableName = "items";
-	
+
+	public function get(Array $criteria, $table) {
+
+		// Set the table to act upon.
+		$table = $this->_setTable($table);
+
+		// Start of the SQL statement.
+		if(array_key_exists("tag_id", $criteria)) {
+			$sql = "SELECT items.* FROM items, tag_items";
+		} else {
+			$sql = "SELECT * FROM {$table}";
+		}
+
+		// If a status isn't passed in, then only get the live (status=1) records.
+		if(!array_key_exists("status", $criteria)) {
+			$criteria["status"] = 1;
+		}
+
+		// Table join.
+		if(array_key_exists("tag_id", $criteria)) {
+			$criteria["items.id"] = "tag_items.item_id";
+		}
+
+		// Update the ID key as there's 2 id fields now new table joined!
+		if(array_key_exists("id", $criteria)) {
+			$criteria["items.id"] = $criteria["id"];
+			unset($criteria["id"]);
+		}
+
+		// If there's criteria, then build the SQL statement.
+		$sql .= $this->_buildWhere($criteria);
+
+		// Execute the query and return the results.
+		return $this->db->query($sql)->fetchAll();
+	}
+
 }
