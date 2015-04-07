294c2e13dd5832ce989f891b6a8dca1eb11c11ac
Edward Casbon <edward@edwardcasbon.co.uk>
2015-04-07 10:57:05 +0100

    Finished attachments controller.

--- app/modules/default/controllers/AttachmentsController.php
+++ app/modules/default/controllers/AttachmentsController.php
@@ -1,66 +1,98 @@
 <?php
 class AttachmentsController extends Bucketlists_Controller {
-	
+
 	public function __construct ($route) {
-		
+
 		// Call the Bucketlists constructor.
 		parent::__construct($route);
-		
+
 		// Create an instance of the model.
-		$this->attachmentsModel = new Attachments($this->db);		
+		$this->attachmentsModel = new Attachments($this->db);
 	}
-	
+
 	public function indexAction () {
 
 		// Validate the HTTP method.
 		$this->validateHttpMethod("GET");
-		
+
 		// Expected / Allowed params.
 		$expectedParams = array(
 			"id",
 			"itemId"
 		);
-		
-		// Get the params	
+
+		// Get the params
 		$params = $this->getExpectedParams($expectedParams);
-				
+
 		// Get the attachments.
 		$attachments = $this->attachmentsModel->get($this->format($params));
-			
+
 		// Output the attachments.
 		$this->view->json = $attachments;
 	}
-	
+
 	public function createAction () {
-		
+
 		// Validate the HTTP method.
 		$this->validateHttpMethod("POST");
-		
+
 		// Format the params.
 		$params = $this->format($this->route->params);
-				
+
 		// Create the attachment.
 		$attachment = $this->attachmentsModel->add($params);
-								
+
 		// Output the attachment.
-		$this->view->json = $attachment;		
+		$this->view->json = $attachment;
 	}
-	
+
 	public function updateAction () {
-		
+
 		// Validate the HTTP method.
 		$this->validateHttpMethod("PUT");
-		
+
+		// Expected / Allowed params.
+		$expectedParams = array(
+			"id"
+		);
+
+		// Get the expected params.
+		$this->getExpectedParams($expectedParams);
+
+		// Get the params.
+		$params = $this->format($this->route->params);
+
+		// Get the criteria (ID).
+		$criteria = array("id" => $params["id"]);
+
+		// Get the data, by removing the ID.
+		unset($params["id"]);
+
+		// Update the attachment.
+		$attachment = $this->attachmentsModel->update($params, $criteria);
+
+		// Output the updated attachment.
+		$this->view->json = $attachment;
 	}
-	
+
 	public function deleteAction () {
-		
+
 		// Validate the HTTP method.
 		$this->validateHttpMethod("DELETE");
-		
-		// Remember to update the status code of the record, rather than delete 
-		// the record entirely!
-		
+
+		// Expected / Allowed params.
+		$expectedParams = array(
+			"id"
+		);
+
+		// Get the expected params.
+		$params = $this->getExpectedParams($expectedParams);
+
+		// Update the attachments status code, rather than delete it completely.
+		$attachment = $this->attachmentsModel->update(array("status" => 0), array("id" => $params["id"]));
+
+		// Output the updated attachment.
+		$this->view->json = $attachment;
 	}
-	
+
 }
